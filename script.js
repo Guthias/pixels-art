92815e80f16b74ddb786c17324d3f8ba8dd13fde
guthias <gmgustavom@gmail.com>
2021-11-25 17:41:43 -0300

    Substituindo arredondamento para math round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateRandomColor() {
-  const green = Math.floor(Math.random() * 255);
-  const red = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const red = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
 
   return `rgb(${red}, ${green}, ${blue})`;
 }
